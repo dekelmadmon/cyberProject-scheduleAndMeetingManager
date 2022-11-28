4ae7e58be87cbda9fea4a527f27a1587f56f49ae
amit <null>
2022-11-28 15:21:26 +0200

    com

--- flaskFiles/scriptsFlask.py
+++ flaskFiles/scriptsFlask.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
-app=Flask(__name__)
+app = Flask(__name__)
+
+
 @app.route('/')
 def home():
    return render_template('theWeb.html')
+
+
+def main_page():
+   return render_template('home.html')
 
 
 if __name__ == '__main__':
