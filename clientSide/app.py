e500c69b0abcf0d18b706b8da8b9b4f056cfd886
dekel494 <52621044+dekel494@users.noreply.github.com>
2023-05-28 15:36:36 +0300

    made it send responses for the functions

--- clientSide/app.py
+++ clientSide/app.py
@@ -1,13 +1,16 @@
-import json
 import datetime
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, jsonify, request
 from src import sqliteDBModule as DBM
 from src import client
+import logging
 
 class MeetingSchedulerApp:
     def __init__(self):
         self.app = Flask(__name__)
+        self.setup_routes()
+        self.setup_logging()
 
+    def setup_routes(self):
         self.app.route('/main')(self.main_page)
         self.app.route('/')(self.home_page)
         self.app.route('/login-page')(self.login_page)
@@ -18,77 +21,106 @@ class MeetingSchedulerApp:
         self.app.route('/update_schedule_dates', methods=["POST"])(self.update_dates)
         self.app.route('/request-meeting', methods=['POST'])(self.request_meeting)
 
+    def setup_logging(self):
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
     def start(self):
         self.app.run(host="127.0.0.1", port=80, debug=True)
 
+    @staticmethod
+    def render_template(template_name):
+        return render_template(template_name)
+
     def main_page(self):
-        return render_template('main.html')
+        return self.render_template('main.html')
 
     def home_page(self):
-        return render_template('home.html')
+        return self.render_template('home.html')
 
     def login_page(self):
-        return render_template("login.html")
+        return self.render_template('login.html')
 
     def sign_in_page(self):
-        return render_template("sign_in.html")
+        return self.render_template('sign_in.html')
+
+    @staticmethod
+    def get_json_data():
+        return request.get_json()
 
     def post_new_activity(self):
-        data = request.data.decode('utf-8')
-        json_data = json.loads(data)
-        return json_data, 200
+        json_data = self.get_json_data()
+        response = jsonify(response='Received new activity: {}'.format(json_data))
+        self.logger.info('Post new activity response: %s', response.json)
+        return response, 200
 
     def login_info(self):
-        data = request.data.decode('utf-8')
-        json_data = json.loads(data)
-        print(json_data)
-        email = json_data['email']
-        password = json_data['password']
+        json_data = self.get_json_data()
+        email = json_data.get('email')
+        password = json_data.get('password')
         db = DBM.Database()
         if db.login_able(email, password):
-            return 'approved', 200
+            response = jsonify(response='Login successful')
+            self.logger.info('Login response: %s', response.json)
+            return response, 200
         else:
-            return "Invalid credentials", 401
+            response = jsonify(response='Invalid credentials')
+            self.logger.info('Login response: %s', response.json)
+            return response, 401
 
     def sign_in_info(self):
-        data = request.data.decode('utf-8')
-        json_data = json.loads(data)
-        username = json_data["username"]
-        email = json_data['email']
-        password = json_data['password']
+        json_data = self.get_json_data()
+        username = json_data.get('username')
+        email = json_data.get('email')
+        password = json_data.get('password')
         db = DBM.Database()
         if db.sign_in(username, email, password):
-            return '', 200
+            response = jsonify(response='Sign-in successful')
+            self.logger.info('Sign-in response: %s', response.json)
+            return response, 200
         else:
-            return 'Invalid credentials or user exists', 401
+            response = jsonify(response='Invalid credentials or user exists')
+            self.logger.info('Sign-in response: %s', response.json)
+            return response, 401
 
     def update_dates(self):
-        try:
-            data = request.data.decode('utf-8')
-            json_data = json.loads(data)
-            last_sunday = self.last_sunday_date()
-            response_data = str(last_sunday + datetime.timedelta(json_data['factor'] - 1))
-            this_date = json.dumps({"data": response_data})
-            return make_response(this_date, 200)
-        except json.JSONDecodeError:
-            return make_response(json.dumps({"error": "Invalid JSON data"}), 400)
-        except KeyError:
-            return make_response(json.dumps({"error": "Missing key in JSON data"}), 400)
+        json_data = self.get_json_data()
+        factor = json_data.get('factor')
+        if factor is None:
+            response = jsonify(error='Missing "factor" in JSON data')
+            self.logger.info('Update dates response: %s', response.json)
+            return response, 400
 
-    def last_sunday_date(self):
+        last_sunday = self.last_sunday_date()
+        response_data = str(last_sunday + datetime.timedelta(factor - 1))
+        response = jsonify(response='Updated dates: {}'.format(response_data))
+        self.logger.info('Update dates response: %s', response.json)
+        return response, 200
+
+    @staticmethod
+    def last_sunday_date():
         today = datetime.date.today()
         days_since_sunday = today.weekday() + 1
         last_sunday = today - datetime.timedelta(days=days_since_sunday)
         return last_sunday
 
     def request_meeting(self):
-        attendee = request.json['attendee']
-        sender = request.json['sender']
-        date = request.json['date']
+        json_data = self.get_json_data()
+        required_fields = ['attendee', 'sender', 'date']
+        if not all(field in json_data for field in required_fields):
+            response = jsonify(error='Missing required data in JSON')
+            self.logger.info('Request meeting response: %s', response.json)
+            return response, 400
+
+        attendee = json_data['attendee']
+        sender = json_data['sender']
+        date = json_data['date']
+
         message = {'type': 'request-meeting', 'attendee': attendee}
         client.request_meeting(attendee, sender, date)
-        # return a response to the client
-        return {'status': 'ok'}
+        response = jsonify(response='Meeting requested successfully')
+        self.logger.info('Request meeting response: %s', response.json)
+        return response, 200
 
 
 if __name__ == '__main__':
