c3386ae53e7bf75f1bd577b5cf9a90144a55afbf
dekel494 <52621044+dekel494@users.noreply.github.com>
2023-06-18 11:01:42 +0300

    full project u fuckers removed uneccecery parts and auto the function for recive meetings

--- clientSide/app.py
+++ clientSide/app.py
@@ -68,7 +68,6 @@ class MeetingSchedulerApp:
         self.app.route('/recieve-meetings', methods=['GET'])(self.recieve_meetings)
         self.app.route('/update-meeting', methods=['POST'])(self.update_meeting)
         self.app.route('/get_email_cookie', methods=['GET'])(self.get_email_cookie)
-        self.app.route('/notify-meeting-request/<sender>/<date>')(self.render_notify_meeting_request)
 
         # ...
 
